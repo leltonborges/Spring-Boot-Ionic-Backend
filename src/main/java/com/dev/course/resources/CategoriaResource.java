acbb899c1347a63941b3952873bdfb8feb407044
Lelton Borges <lelton.borges1@gmail.com>
2020-04-23 20:57:38 -0300

    Update Category with PUT

--- src/main/java/com/dev/course/resources/CategoriaResource.java
+++ src/main/java/com/dev/course/resources/CategoriaResource.java
@@ -24,7 +24,7 @@ public class CategoriaResource {
 	private CategoryService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Category> find(@PathVariable Integer id) {
 		Category obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
@@ -36,4 +36,10 @@ public class CategoriaResource {
 		return ResponseEntity.created(uri).build();
 	}
 
+	@RequestMapping(method = RequestMethod.PUT, value = "{id}")
+	public ResponseEntity<Void> update(@RequestBody Category obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/dev/course/services/CategoryService.java
+++ src/main/java/com/dev/course/services/CategoryService.java
@@ -24,4 +24,9 @@ public class CategoryService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+	
+	public Category update(Category obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 }
