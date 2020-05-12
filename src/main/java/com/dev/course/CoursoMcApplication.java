981804f5f345193f8f1a9da3f3dd3013b791daf7
Lelton Borges <lelton.borges1@gmail.com>
2020-05-12 00:50:24 -0300

    Saving first file to S3 Amazon

--- src/main/java/com/dev/course/CoursoMcApplication.java
+++ src/main/java/com/dev/course/CoursoMcApplication.java
@@ -1,12 +1,17 @@
 package com.dev.course;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.dev.course.services.S3Service;
+
 @SpringBootApplication
 public class CoursoMcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CoursoMcApplication.class, args);
@@ -14,5 +19,6 @@ public class CoursoMcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("/home/lelton/Imagens/honeycombs.jpg");
 	}
 }
