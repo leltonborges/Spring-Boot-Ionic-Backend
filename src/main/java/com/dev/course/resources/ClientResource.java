225e9a51e58ffb1aac187bbc5e44cfd35a07c316
Lelton Borges <lelton.borges1@gmail.com>
2020-05-12 23:41:02 -0300

    Upload image via endpoint

--- src/main/java/com/dev/course/resources/ClientResource.java
+++ src/main/java/com/dev/course/resources/ClientResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dev.course.domain.Client;
@@ -28,54 +29,58 @@ import com.dev.course.services.ClientService;
 public class ClientResource {
 	@Autowired
 	private ClientService service;
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Client> findById(@PathVariable Integer id){
+	public ResponseEntity<Client> findById(@PathVariable Integer id) {
 		Client obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClientDTO objDto, @PathVariable Integer id) {
 		Client obj = service.fromClient(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody ClientNewDTO cliDTO){
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClientNewDTO cliDTO) {
 		Client cli = service.fromClient(cliDTO);
 		cli = service.insert(cli);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(cli.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<ClientDTO>> findAll(){
+	public ResponseEntity<List<ClientDTO>> findAll() {
 		List<Client> list = service.findAll();
 		List<ClientDTO> cliDTO = list.stream().map(obj -> new ClientDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(cliDTO);
 	}
-	
+
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Integer id){
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET, value = "/page")
-	public ResponseEntity<Page<ClientDTO>> findPage(
-			@RequestParam(value = "page", defaultValue = "0") Integer page,
+	public ResponseEntity<Page<ClientDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
 			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction){
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
 		Page<Client> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClientDTO> listDTO = list.map(obj -> new ClientDTO(obj));
-		
+
 		return ResponseEntity.ok().body(listDTO);
-		
+	}
+
+	@RequestMapping(value = "/picture", method = RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name = "file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/dev/course/services/ClientService.java
+++ src/main/java/com/dev/course/services/ClientService.java
@@ -1,5 +1,6 @@
 package com.dev.course.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.dev.course.domain.Address;
 import com.dev.course.domain.City;
@@ -34,6 +36,8 @@ public class ClientService {
 	private AddressRepository addressRepository;
 	@Autowired
 	private BCryptPasswordEncoder passEncoder;
+	@Autowired
+	private S3Service s3Service;
 	
 	public Client find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -104,5 +108,9 @@ public class ClientService {
 	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
